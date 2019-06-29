99df86497d40ecc3b8555b2b85327e8fd7eda757
duydodevgithub <duydo.dev@gmail.com>
2019-06-28 23:03:10 -0500

    add view more info

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 
-//SCHEMA SET UP
+// SCHEMA SET UP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 // var campgrounds = [
-//     {name: "Campsite 1", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 2", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 3", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 4", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 5", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 6", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//     {name: "Campsite 7", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+//     {name: "Campsite 1", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 2", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 3", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 4", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 5", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 6", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
+//     {name: "Campsite 7", image:"https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg", description: "This is description"},
 // ];
 
 // campgrounds.forEach(function(element){
@@ -54,7 +55,7 @@ app.get("/campgrounds", (req, res)=>{
             console.log(err);
         } else {
             console.log(allCampgrounds);
-            res.render("campgrounds", {campgrounds: allCampgrounds})    
+            res.render("index", {campgrounds: allCampgrounds})    
         }
     })
 })
@@ -63,15 +64,28 @@ app.get("/campgrounds/new", (req, res)=>{
     res.render("new");
 })
 
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampGround){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(foundCampGround);
+            res.render("show", {campgrounds: foundCampGround});
+        }
+    })
+})
+
 
 app.post("/campgrounds", (req, res)=>{
     let name = req.body.name;
     let image = req.body.image;
-    let newCampground = {name: name, image: image};
+    let description = req.body.description;
+    let newCampground = {name: name, image: image, description: description};
     Campground.create(newCampground, function(err, data){
         if(err) {
             console.log(err);
         } else {
+            // console.log(data);
             res.redirect("/campgrounds");
         }
     })
